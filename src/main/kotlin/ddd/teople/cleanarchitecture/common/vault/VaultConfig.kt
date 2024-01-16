32f051a2a4e6a4ac1b60e35a1efed17bffb95e25
ParkYunHo <qkrdbsgh0921@gmail.com>
2024-01-16 23:04:28 +0900

    :sparkles:feature : Vault Config 설정

--- src/main/kotlin/ddd/teople/cleanarchitecture/common/vault/VaultConfig.kt
+++ src/main/kotlin/ddd/teople/cleanarchitecture/common/vault/VaultConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Primary
 import org.springframework.vault.authentication.AppRoleAuthentication
 import org.springframework.vault.authentication.AppRoleAuthenticationOptions
 import org.springframework.vault.authentication.ClientAuthentication
+import org.springframework.vault.authentication.TokenAuthentication
 import org.springframework.vault.client.VaultEndpoint
 import org.springframework.vault.config.AbstractVaultConfiguration
 import java.net.URI
@@ -18,20 +19,23 @@ class VaultConfig : AbstractVaultConfiguration() {
     }
 
     override fun clientAuthentication(): ClientAuthentication {
-        // AppRole
-        val options = AppRoleAuthenticationOptions.builder()
-            .roleId(
-                AppRoleAuthenticationOptions.RoleId.provided(
-                    environment.getProperty("vault.app-role.role-id") ?: throw NullPointerException("vault role-id가 없습니다.")
-                )
-            )
-            .secretId(
-                AppRoleAuthenticationOptions.SecretId.provided(
-                    environment.getProperty("vault.app-role.secret-id") ?: throw NullPointerException("vault secret-id가 없습니다.")
-                )
-            )
-            .build()
 
-            return AppRoleAuthentication(options, restOperations())
+        return TokenAuthentication(environment.getProperty("vault.token") ?: throw NullPointerException("vault token이 없습니다."))
+
+//        // AppRole
+//        val options = AppRoleAuthenticationOptions.builder()
+//            .roleId(
+//                AppRoleAuthenticationOptions.RoleId.provided(
+//                    environment.getProperty("vault.app-role.role-id") ?: throw NullPointerException("vault role-id가 없습니다.")
+//                )
+//            )
+//            .secretId(
+//                AppRoleAuthenticationOptions.SecretId.provided(
+//                    environment.getProperty("vault.app-role.secret-id") ?: throw NullPointerException("vault secret-id가 없습니다.")
+//                )
+//            )
+//            .build()
+//
+//            return AppRoleAuthentication(options, restOperations())
     }
 }
